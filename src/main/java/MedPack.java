8bbaab85ece9399a8438c182e14ca778ad7fde32
kaylamf25 <46701855+kaylamf25@users.noreply.github.com>
2019-10-12 15:38:54 -0500

    MedPack update

--- src/main/java/MedPack.java
+++ src/main/java/MedPack.java
@@ -7,29 +7,27 @@ public class MedPack
     private int packageNum;
     private ArrayList<PackItem> contents;
 
-    private final static ArrayList<PackItem> pack2Items = new ArrayList<PackItem>(){
-    {
-        //possible pack items with approximate price and weight
-        add(new PackItem("Medical Tape", 35.7,2));
-        PackItem scissors = new PackItem("Scissors", 0,0);
-        PackItem tweezers = new PackItem("Tweezers",0,0);
-        PackItem hydroPerox = new PackItem("Hydrogen Peroxide",0,0);
-        PackItem coldComp = new PackItem("Instant Cold Compress",0,0);
-        PackItem gauze = new PackItem("Gauze Pads",0,0);
-        PackItem adhesiveBandages = new PackItem("Adhesive Bandages", 0,0);
-        PackItem blanket = new PackItem("Thermal Blanket", 0,0);
-        PackItem gloves = new PackItem("Non-Latex Gloves",0,0);}
-    };
-
     private final static ArrayList<PackItem> pack1Items = new ArrayList<PackItem>(){
         {
-
+            //possible pack items with approximate price and weight
+            add(new PackItem("Medical Tape", 35.7,2));
+            PackItem scissors = new PackItem("Scissors", 0,0);
+            PackItem tweezers = new PackItem("Tweezers",0,0);
+            PackItem hydroPerox = new PackItem("Hydrogen Peroxide",0,0);
+            PackItem coldComp = new PackItem("Instant Cold Compress",0,0);
+            PackItem gauze = new PackItem("Gauze Pads",0,0);
+            PackItem adhesiveBandages = new PackItem("Adhesive Bandages", 0,0);
+            PackItem blanket = new PackItem("Thermal Blanket", 0,0);
+            PackItem gloves = new PackItem("Non-Latex Gloves",0,0);
         }
     };
 
     private final static ArrayList<PackItem> pack0Items = new ArrayList<PackItem>(){
         {
             PackItem aed = new PackItem("AED", 0, 0);
+            PackItem epipen = new PackItem("EpiPen",0,0);
+            PackItem inhaler = new PackItem("Inhaler", 0,0);
+            PackItem cprMask = new PackItem("cprMask",0,0);
         }
     };
 
@@ -38,7 +36,6 @@ public class MedPack
         packageNum = 0;
         contents.addAll(pack0Items);
         contents.addAll(pack1Items);
-        contents.addAll(pack2Items);
     }
 
     public MedPack(int packageNum)
@@ -51,13 +48,10 @@ public class MedPack
                 contents.addAll(pack0Items);
             case 1:
                 contents.addAll(pack1Items);
-            case 2:
-                contents.addAll(pack2Items);
                 break;
             default:
                 contents.addAll(pack0Items);
                 contents.addAll(pack1Items);
-                contents.addAll(pack2Items);
                 break;
         }
     }
