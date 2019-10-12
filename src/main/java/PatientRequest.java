2276366e3713593d9bb519ac9c91196d6c88e187
nunruh <nathaniel-unruh@uiowa.edu>
2019-10-12 15:40:22 -0500

    Working on DroneRequests, closestDrone implemented. Needs medpack validation

--- src/main/java/PatientRequest.java
+++ src/main/java/PatientRequest.java
@@ -1,9 +1,18 @@
+import com.mapbox.api.geocoding.v5.MapboxGeocoding;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
 import com.mapbox.geojson.Point;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.util.List;
 
 public class PatientRequest {
     private Point location;
     private int type;
 
+    private static final String accessToken = "pk.eyJ1IjoibmF0ZXVucnVoIiwiYSI6ImNrMW5tODU2ZDAyM2EzZHBmeWYzNW9oNWwifQ.wMcNUd8CHVB6c_DIv6lXHg";
     PatientRequest(Point location, int type){
         this.location = location;
         this.type = type;
@@ -13,6 +22,63 @@ public class PatientRequest {
         this.location = location;
         this.type = 0;
     }
+    PatientRequest(String request){
+        MapboxGeocoding mapboxGeocoding = MapboxGeocoding.builder().accessToken(accessToken).query(request).build();
+        mapboxGeocoding.enqueueCall(new Callback<GeocodingResponse>() {
+            @Override
+            public void onResponse(retrofit2.Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+                List<CarmenFeature> results = response.body().features();
+
+                if (results.size() > 0) {
+
+                    // Log the first results Point.
+                    Point firstResultPoint = results.get(0).center();
+                    location = firstResultPoint;
+
+                } else {
+
+                    // No result for your request were found.
+                    throw new IllegalArgumentException("Invalid request.");
+                }
+            }
+
+
+            @Override
+            public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
+
+    }
+
+    PatientRequest(String request, int type){
+        MapboxGeocoding mapboxGeocoding = MapboxGeocoding.builder().accessToken("pk.eyJ1IjoibmF0ZXVucnVoIiwiYSI6ImNrMW5tODU2ZDAyM2EzZHBmeWYzNW9oNWwifQ.wMcNUd8CHVB6c_DIv6lXHg").query(request).build();
+        mapboxGeocoding.enqueueCall(new Callback<GeocodingResponse>() {
+            @Override
+            public void onResponse(retrofit2.Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+                List<CarmenFeature> results = response.body().features();
+
+                if (results.size() > 0) {
+
+                    // Log the first results Point.
+                    Point firstResultPoint = results.get(0).center();
+                    location = firstResultPoint;
+
+                } else {
+
+                    // No result for your request were found.
+                    throw new IllegalArgumentException("Invalid request.");
+                }
+            }
+
+
+            @Override
+            public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
+        this.type = type;
+    }
 
     public Point getLocation(){
         return this.location;
@@ -21,4 +87,6 @@ public class PatientRequest {
     public int getType(){
         return type;
     }
+
+
 }
