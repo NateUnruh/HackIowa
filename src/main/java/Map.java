2b3e9800d75206f6e1d1af1a8c6388f4b2df7c16
nunruh <nathaniel-unruh@uiowa.edu>
2019-10-12 10:06:40 -0500

    This doesn't wanna work

---
+++ src/main/java/Map.java
@@ -0,0 +1,15 @@
+import com.mapbox.geojson.Point;
+import com.mapbox.turf.TurfConstants;
+import com.mapbox.turf.TurfMeasurement;
+import com.mapbox.turf.TurfMeta;
+
+public class Map {
+
+    // Create a GeoJSON point representation of the locations.
+    private static final Point TOWER_BRIDGE = Point.fromLngLat(-0.07515, 51.50551);
+    private static final Point LONDON_EYE = Point.fromLngLat(-0.12043, 51.50348);
+
+    // Run the points through the Turf Measurement method and receive the distance.
+
+
+}
