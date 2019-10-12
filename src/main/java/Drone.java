d01163c414098fe8d3120f4c4d3efada4658e3ca
Addison Armstrong <addison-armstrong@uiowa.edu>
2019-10-12 13:28:37 -0500

    Updating Drone

--- src/main/java/Drone.java
+++ src/main/java/Drone.java
@@ -3,16 +3,30 @@ import com.mapbox.turf.TurfConstants;
 import com.mapbox.turf.TurfMeasurement;
 
 public class Drone {
-    private static final double mph = 60.0;
+    private double mph = 60.0;
     private Point coords;
-    private double batteryLife;
+    private double batteryLife = 25;
     private MedPack medPack;
-    private boolean home;
+    private boolean home = false;
+    private double maxPayLoad = 5; //measured in pounds
+    private double operatingTempLow = 0;
+    private double operatingTempHigh= 0;
+
+
     Drone(Point coords, MedPack medPack){
         this.coords = coords;
         this.medPack = medPack;
-        this.batteryLife = 25;
         this.home = true;
+    }
+
+    Drone(Point coords, MedPack medPack, double mph, double maxPayLoad, double operatingTempLow, double operatingTempHigh){
+        this.coords = coords;
+        this.medPack = medPack;
+        this.home = true;
+        this.mph = mph;
+        this.maxPayLoad = maxPayLoad;
+        this.operatingTempLow = operatingTempLow;
+        this.operatingTempHigh = operatingTempHigh;
     }
 
     // TODO: Change to call coords
@@ -35,6 +49,9 @@ public class Drone {
         this.coords = dest;
     }
 
+
+
+    //testing main
     public static void main(String[] args){
         Drone d = new Drone(Point.fromLngLat(-91.532788,41.651897),new MedPack());
         System.out.println(d.timeToArrival(Point.fromLngLat(-91.5192,41.6765)));
