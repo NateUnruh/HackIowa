244892de4024cd98d1cd02f33a37f32dc793d20c
nunruh <nathaniel-unruh@uiowa.edu>
2019-10-13 11:46:08 -0500

    Google API >> Mapbox API

--- src/main/java/Drone.java
+++ src/main/java/Drone.java
@@ -1,10 +1,18 @@
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.GeocodingApiRequest;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
 import com.mapbox.geojson.Point;
 import com.mapbox.turf.TurfConstants;
 import com.mapbox.turf.TurfMeasurement;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Drone{
+    private static final String APIkey =  "AIzaSyDZ-Kk-itsqd04Z5hJtmndjlYa1teL170s";
     private double mph = 30.0; // in mph
     private Point homeCoords;
     private Point coords;
@@ -93,6 +101,11 @@ public class Drone{
         this.maxPayLoad = maxPayLoad;
     }
 
+    public String getAddresss() throws InterruptedException, ApiException, IOException {
+        GeocodingResult[] results = GeocodingApi.newRequest(new GeoApiContext.Builder().apiKey(APIkey).build()).latlng(new LatLng(coords.longitude(),coords.latitude())).await();
+        return results[0].formattedAddress;
+    }
+
     public void move(Point dest){
         home = false;
         batteryLife -= distanceTo(dest);
@@ -102,12 +115,13 @@ public class Drone{
 
 
     //testing main
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException, ApiException, IOException {
 //        Drone d = new Drone(Point.fromLngLat(-91.532788,41.651897),new MedPack(2));
         DroneForce Array = new DroneForce();
         ArrayList<Drone> ArrayList = Array.getDronesList();
         for(Drone drone : ArrayList) {
             System.out.println(drone.timeToArrival(Point.fromLngLat(-91.5192, 41.6765)));
         }
+        System.out.println(ArrayList.get(0).getAddresss());
     }
 }
