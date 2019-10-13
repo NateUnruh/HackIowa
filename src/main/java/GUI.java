b2d658d192eb00e2ded25653973a9f3fd14af99a
Ram <ramteja-sajja@uiowa.edu>
2019-10-13 11:49:10 -0500

    Merge branch 'master' of C:\Users\rsajja\Desktop\GIT......LOVE with conflicts.

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -9,12 +9,16 @@ import javax.swing.JTextField;
 import javax.swing.JOptionPane;
 
 public class GUI extends JFrame {
+    private DroneRequest Closest;
     private JLabel label1;
     private JLabel label2;
     private JTextField Location;
     private JComboBox PackType;
     private JButton Send;
     private JPanel panel;
+    private JLabel TOaddress;
+    private JLabel Fromaddress;
+    private JLabel ETA;
 
     public GUI(){
         setTitle("HELP!!!!");
@@ -25,20 +29,28 @@ public class GUI extends JFrame {
         Location = new JTextField("",30);
         PackType = new JComboBox<>(options);
         Send = new JButton("Send Request");
+        TOaddress = new JLabel("");
+        Fromaddress = new JLabel("");
+        Closest = new DroneRequest();
+        ETA = new JLabel("");
         panel = new JPanel();
         panel.add(label1);
         panel.add(Location);
         panel.add(label2);
         panel.add(PackType);
         panel.add(Send);
+        panel.add(TOaddress);
+        panel.add(Fromaddress);
+        panel.add(ETA);
 
         Send.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                dispose();
-                JOptionPane.showMessageDialog(null,"Your Help is on the way! Hold it there",null,JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(null,"Your Help is on the way! Hang in there",null,JOptionPane.PLAIN_MESSAGE);
                 PatientRequestGoogleAPI address = null;
+                Map temp =null;
                 try {
+
                     address = new PatientRequestGoogleAPI(Location.getText());
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -47,9 +59,14 @@ public class GUI extends JFrame {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-                System.out.println(address.getLocation());
+                    TOaddress.setText("To: " + Location.getText() +"     ");
+                    Fromaddress.setText("Drone sent From: " + "");
+                    ETA.setText("ETA: " + address.timeToArrival(Closest.closestDrone(address.getLocation())));
+                    Send.setEnabled(false);
+                    Location.setEnabled(false);
+                    PackType.setEnabled(false);
             }
+
         });
 
         add(panel);
@@ -57,7 +74,7 @@ public class GUI extends JFrame {
 
     public static void main(String[] args) {
         GUI temp = new GUI();
-        temp.setSize(400,200);
+        temp.setSize(400,300);
         temp.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
